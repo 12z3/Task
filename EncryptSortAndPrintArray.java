d1e8545246b3439f3645d74b66001a23132a5327
12z3 <77529497+12z3@users.noreply.github.com>
2022-08-18 18:36:48 +0300

    Add files via upload

--- EncryptSortAndPrintArray.java
+++ EncryptSortAndPrintArray.java
@@ -7,7 +7,28 @@ public class EncryptSortAndPrintArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Unsolved !!!
+        /**
+         * @Problem: Write a program that reads a sequence of strings from the console.
+         * Encrypt every string by summing:
+         * •	The code of each vowel multiplied by the string length.
+         * •	The code of each consonant is divided by the string length.
+         * Sort the number sequence in ascending order and print it on the console.
+         * On the first line, you will always receive the number of strings you must read.
+         * Examples
+         Input	        Output	 Comments
+         * 4
+         * Peter        1032     Peter = 1071
+         * Maria        1071     Maria = 1532
+         * Katya        1168     Katya = 1032
+         * Todor	    1532     Todor = 1168
+
+         * 3
+         * Sofia        1396     Sofia = 1601
+         * London       1601     London = 1396
+         * Washington	3202     Washington = 3202
+         */
+
+        // Unsolved!!!
 
         char[] letters = {'a', 'e', 'i', 'o', 'u', 'y'};
         int n = scanner.nextInt();
