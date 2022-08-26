fe68d0343002504dfde22778d1df627635f2358d
12z3 <77529497+12z3@users.noreply.github.com>
2022-08-26 19:17:24 +0300

    Add files via upload

--- aboutDigits.java
+++ aboutDigits.java
@@ -9,19 +9,19 @@ public class aboutDigits {
         int number = 12345;
         int x = number, digit = 0, digit1 = 0;
 
-        while ( number > 0){
+        while (number > 0) {
             digit = (digit * 10) + number % 10;
-            number = number/10;
+            number = number / 10;
         }
         System.out.println(x + " -> " + digit);
 
         String[] numbers = String.valueOf(number).split("");
         System.out.print("numbers[] = ");
 
-        for (String el: numbers) System.out.print(el);
+        for (String el : numbers) System.out.print(el);
         System.out.println();
 
-        for (String el: numbers){
+        for (String el : numbers) {
             digit1 = (digit1 * 10) + Integer.parseInt(el);
         }
         System.out.println("digit = " + digit1);
