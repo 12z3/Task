1a71328b8dcf18fde12a58d873642f74eb6132e4
12z3 <77529497+12z3@users.noreply.github.com>
2022-10-01 18:04:22 +0300

    Add files via upload

--- Randomizing.java
+++ Randomizing.java
@@ -10,8 +10,8 @@ public class Randomizing {
         randomizing(list);
     }
 
-    private static boolean isCheck(List<Integer> numbers, int digit){
-        for (int el: numbers) {
+    private static boolean isCheck(List<Integer> numbers, int digit) {
+        for (int el : numbers) {
             if (el == digit) return true;
         }
         return false;
@@ -19,17 +19,16 @@ public class Randomizing {
 
     private static void randomizing(List<Integer> list) {
         List<Integer> numbers = new ArrayList<>();
-        int min = 0, count = 0, previously = 0, allCounts = 0;
-        int max = list.size() - 1;
+        int min = 0, count = 0,
+                previously = 0, max = list.size() - 1;
 
-        while (true){
-            int random = (int)(Math.random() * ((max - min) + 1)) + min;
+        while (true) {
+            int random = (int) (Math.random() * ((max - min) + 1)) + min;
 
             if (!isCheck(numbers, list.get(random))) {
                 System.out.print(list.get(random) + " ");
                 numbers.add(list.get(random));
                 count++;
-                allCounts++;
                 if (count == list.size()) return;
             }
         }
