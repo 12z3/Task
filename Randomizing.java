47358dedad77fbedd2600ba94ecbbbf32dc243df
12z3 <77529497+12z3@users.noreply.github.com>
2022-10-01 18:39:54 +0300

    Add files via upload

--- Randomizing.java
+++ Randomizing.java
@@ -4,17 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Randomizing {
+    /**
+     * @Problem: <a href="https://www.youtube.com/watch?v=H57pSYrPv-g">...</a>
+     */
+
     public static void main(String[] args) {
 
         List<Integer> list = new ArrayList<>(List.of(10, 5, 56, 8, 98));
         randomizing(list);
-    }
-
-    private static boolean isMatch(List<Integer> numbers, int digit) {
-        for (int el : numbers) {
-            if (el == digit) return true;
-        }
-        return false;
+        System.out.println();
+        shifting(list);
     }
 
     private static void randomizing(List<Integer> list) {
@@ -31,5 +30,25 @@ public class Randomizing {
                 if (stopCriteria == list.size()) return;
             }
         }
+    }
+
+    private static boolean isMatch(List<Integer> numbers, int digit) {
+        for (int el : numbers) {
+            if (el == digit) return true;
+        }
+        return false;
+    }
+
+    private static void shifting(List<Integer> list) {
+        int min = 0, max = list.size() - 1;
+        int randomIndex = (int) (Math.random() * ((max - min) + 1)) + min;
+
+        for (int i = 0; i < list.size(); i++) {
+            int tmp = list.get(i);
+            list.set(i, list.get(randomIndex));
+            list.set(randomIndex, tmp);
+        }
+
+        for (int el : list) System.out.print(el + " ");
     }
 }
