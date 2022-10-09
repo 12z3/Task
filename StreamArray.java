9379a5bdb9fc8bf5c10dd02df576a1fff59b7875
12z3 <77529497+12z3@users.noreply.github.com>
2022-10-09 10:09:47 +0300

    Add files via upload

--- StreamArray.java
+++ StreamArray.java
@@ -1,6 +1,7 @@
 package task;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class StreamArray {
 
@@ -14,21 +15,36 @@ public class StreamArray {
 
 
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr1 = {1, 2, 13, 4, 5};
         int[] arr3 = {1, 2, 3, 4, 5};
         int[] arr2 = {13, 2, 23, 14, 45};
 
-        String[] arr11 = {"1", "2", "3", "4", "5"};
+        String[] arr11 = {"1", "2", "13", "4", "5"};
         String[] arr31 = {"1", "2", "3", "4", "5"};
+
+        Integer[] arr1G = {1, 2, 13, 4, 5};
+        Integer[] arr2G = {1, 2, 3, 4, 5};
 
         arraysComparator(arr1, arr2);
         streamArraysComparator(arr11, arr31);
+        anotherComparator();
+    }
+
+    private static void anotherComparator(){
+        Scanner scanner = new Scanner(System.in);
+        String[] arr11 = scanner.nextLine().split(" ");
+        String[] arr21 = scanner.nextLine().split(" ");
+
+        Arrays.sort(arr11);
+        Arrays.sort(arr21);
+        System.out.println(Arrays.equals(arr11,arr21) ? "YES" : "NO");
     }
 
     private static void streamArraysComparator(String[] arr11, String[] arr31) {
         int[] arr1Sorted = Arrays.stream(arr11).mapToInt(Integer::parseInt).sorted().toArray();
         int[] arr2Sorted = Arrays.stream(arr31).mapToInt(Integer::parseInt).sorted().toArray();
         System.out.println(Arrays.equals(arr1Sorted, arr2Sorted) ? "YES" : "NO");
+        System.out.println();
     }
 
     private static void arraysComparator(int[] arr1, int[] arr2) {
@@ -52,5 +68,6 @@ public class StreamArray {
         if (allCounts == arr1.length) {
             System.out.println("YES");
         } else System.out.print("NO");
+        System.out.println();
     }
 }
