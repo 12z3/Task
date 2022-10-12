335dc1ab08cb385acef8e1c53bcb7a8a4b1010e1
12z3 <77529497+12z3@users.noreply.github.com>
2022-10-12 15:32:08 +0300

    Add files via upload

--- Toto.java
+++ Toto.java
@@ -6,8 +6,8 @@ import java.util.Random;
 
 public class Toto {
     /**
-     * Генерира числата от 1 до 49 variants пъти. и Записва резултатите в List<List<Integer>> allNumbers.
-     * Търси колко пъти се намира числото number в резултата.
+     * Генерира числата от 1 до 49 "variants" пъти. и Записва резултатите в "List<List<Integer>> allNumbers".
+     * Търси колко пъти се намира числото "thisNumber" в резултата.
      */
 
     public static void main(String[] args) {
