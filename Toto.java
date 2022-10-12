67ccc15657dadd3977f5041f544e78e6d2c2159a
12z3 <77529497+12z3@users.noreply.github.com>
2022-10-12 14:18:08 +0300

    Add files via upload

--- Toto.java
+++ Toto.java
@@ -5,18 +5,25 @@ import java.util.List;
 import java.util.Random;
 
 public class Toto {
+    /**
+     * Генерира числата от 1 до 49 variants пъти. и Записва резултатите в List<List<Integer>> allNumbers.
+     * Търси колко пъти се намира числото number в резултата.
+     *
+     * @param args
+     */
+
     public static void main(String[] args) {
         generateTotoNum(6, 44);
     }
 
     private static void generateTotoNum(int variants, int number) {
         Random rnd = new Random();
-        number = 44;
+        int number = 44;
         List<Integer> list = new ArrayList<>();
         List<List<Integer>> allNumbers = new ArrayList<>();
 
         int counter = 0;
-        while (counter < variants){
+        while (counter < variants) {
             list = new ArrayList<>();
             for (int k = 0; k < 6; k++) {
                 list.add(rnd.nextInt(50));
@@ -26,10 +33,10 @@ public class Toto {
         }
         System.out.println(allNumbers);
         System.out.println();
-        System.out.println("Numbers " + number + " contains " + allConsist(allNumbers,number) + " times.");
+        System.out.println("Numbers " + number + " contains " + allConsist(allNumbers, number) + " times.");
     }
 
-    private static boolean isConsist(List<Integer> list, int number){
+    private static boolean isConsist(List<Integer> list, int number) {
         if (list.contains(number)) {
             List<Integer> newList = new ArrayList<>(list);
             System.out.println(newList);
@@ -38,7 +45,7 @@ public class Toto {
         return false;
     }
 
-    private static int allConsist(List<List<Integer>> allNumbers, int number){
+    private static int allConsist(List<List<Integer>> allNumbers, int number) {
         int countConsist = 0;
         for (int i = 0; i < allNumbers.size(); i++) {
             if (isConsist(allNumbers.get(i), number)) countConsist++;
