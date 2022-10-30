ab42d0966790b890d4b2a1ef2fbd2f7d7f6299f1
12z3 <77529497+12z3@users.noreply.github.com>
2022-10-30 12:35:44 +0200

    Add files via upload

--- BubbleSortingVariants.java
+++ BubbleSortingVariants.java
@@ -5,17 +5,22 @@ import java.util.Arrays;
 public class BubbleSortingVariants {
     public static void main(String[] args) {
         int[] arr = {12, 3, 4, 56, 7, 8};
-        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arr));
+//
+//        bubbleForCycle(arr);
+//        System.out.println(Arrays.toString(arr));
+//        System.out.println();
+//
+//        int[] brr = {122, 13, 444, 56, 7, 8};
+//        System.out.println(Arrays.toString(brr));
+//
+//        bubbleWhileCycle(brr);
+//        System.out.println(Arrays.toString(brr));
 
-        bubbleForCycle(arr);
-        System.out.println(Arrays.toString(arr));
-        System.out.println();
+        BubbleSortingVariants ob = new BubbleSortingVariants();
+        ob.insertionSort(arr);
 
-        int[] brr = {122, 13, 444, 56, 7, 8};
-        System.out.println(Arrays.toString(brr));
-
-        bubbleWhileCycle(brr);
-        System.out.println(Arrays.toString(brr));
+        printArray(arr);
     }
 
     private static void bubbleWhileCycle(int[] brr) {
@@ -44,5 +49,32 @@ public class BubbleSortingVariants {
                 }
             }
         }
+    }
+
+
+    void insertionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    /* A utility function to print array of size n*/
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
     }
 }
