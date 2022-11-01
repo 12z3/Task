723318f84a7162ef6583b42c1bc12794adc9322d
12z3 <77529497+12z3@users.noreply.github.com>
2022-11-01 12:54:01 +0200

    Add files via upload

--- Chickens.java
+++ Chickens.java
@@ -4,6 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Chickens {
+
+    /**
+     * @Source: <a href="https://practice.geeksforgeeks.org/problems/chicks-in-a-zoo1159/1">...</a>
+     * @Problem: Initially, the zoo have a single chick.
+     * A chick gives birth to 2 chicks every day and the life expectancy of a chick is 6 days.
+     * Zoo officials want to buy food for chicks so they want to know the number of chicks on an Nth day.
+     * Help the officials with this task.
+     */
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.println(chickens(i));
@@ -14,8 +22,7 @@ public class Chickens {
 
 
     private static long chickens(int day) {
-        long oldChickens = 1, newChicks = 0, allChicks = 0, deathChicks = 0;
-        long oldTime = 1, newTime = 0, rec = 0;
+        long oldChickens = 1, newChicks = 0, allChicks = 0, death = 0;
         Map<Integer, Long> chicks = new HashMap<>();
         int days = 1;
 
@@ -23,19 +30,16 @@ public class Chickens {
         if (day == 1) allChicks = oldChickens;
 
         for (int i = 2; i <= day; i++) {
-            oldChickens -= rec;
+            oldChickens -= death;
             newChicks = oldChickens * 2;
             allChicks = (newChicks + oldChickens);
             oldChickens += newChicks;
 
-            oldTime++;
-            chicks.put(i,newChicks);
+            chicks.put(i, newChicks);
 
             int key = i - 5;
-            if (key >= 1){
-                rec = chicks.get(key);
-                //chicks.remove(key + 1);
-            }
+            if (key >= 1) death = chicks.get(key);
+
             // if (i == day) System.out.println(chicks.get(i));
         }
         return allChicks;
