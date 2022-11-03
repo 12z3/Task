bac919d388ad21092420408f37d50a2f80469d6f
12z3 <77529497+12z3@users.noreply.github.com>
2022-11-03 18:54:13 +0200

    Add files via upload

--- Median.java
+++ Median.java
@@ -3,26 +3,33 @@ package task;
 import java.util.Arrays;
 
 public class Median extends Sorting {
+
+    /**
+     * @Problem: <a href="https://practice.geeksforgeeks.org/problems/median-in-a-row-wise-sorted-matrix1527/1">...</a>
+     * Given a row wise sorted matrix of size R*C where R and C are always odd, find the median of the matrix.
+     */
+
     public static void main(String[] args) {
 
         int[][] input = {{1, 3, 5}, {2, 6, 9}, {3, 6, 9}};
-        int[][] input1 = {{1}, {2}, {3}};
+        int[][] input1 = {{1}, {2}, {3}};                         //  row = 3, col = 1;
         int row = 3, col = 3;
+       // int row = 1, col = 3;
 
         System.out.println(median(input, row, col));
     }
 
     private static int median(int[][] matrix, int R, int C) {
-        int index = -1, elements = 0, median = 0;
+        int index = -1, matrixSize = 0, median = 0;
 
         for (int row1 = 0; row1 < matrix.length; row1++) {
             for (int col1 = 0; col1 < matrix[row1].length; col1++) {
-                elements++;
+                matrixSize++;
             }
         }
 
-        int[] lsArr = new int[elements];
-        if (elements <= 3) {
+        int[] lsArr = new int[matrixSize];
+        if (matrixSize <= 3) {
             int j = C - 1;
             for (int i = 0; i < R; i++) {
                 index++;
