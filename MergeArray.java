bf83791d5ed93fd357d490f1d6ac2d775fe58257
12z3 <77529497+12z3@users.noreply.github.com>
2022-11-06 13:06:38 +0200

    Add files via upload

---
+++ MergeArray.java
@@ -0,0 +1,63 @@
+package task;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class MergeArray {
+    public static void main(String[] args) throws IOException {
+
+        int[] a = {6, 7, 8, 1, 2, 3, 4};
+        int[] b = {1, 2, 5, 7, 8};
+        mergeArray(a, b);
+        mergeArray1(a, b);
+    }
+
+    private static void mergeArray(int[] a, int[] b) {
+        int[] newArr = new int[a.length + b.length];
+        int sizeA = a.length;
+
+        for (int idxA = 0, idxB = (-a.length); idxA < newArr.length; idxA++, idxB++) {
+            if (idxA < a.length) {
+                newArr[idxA] = a[idxA];
+            } else {
+                newArr[sizeA++] = b[idxB];
+            }
+        }
+
+        System.out.println(Arrays.toString(newArr));
+        System.out.printf("Length: a-%d; b-%d;%n", a.length, b.length);
+    }
+
+    private static void mergeArray1(int[] a, int[] b) {
+        int[] newArr = new int[a.length + b.length];
+        int sizeA = a.length;
+
+        for (int idx = 0; idx < a.length; idx++) {
+            newArr[idx] = a[idx];
+        }
+        for (int idx = sizeA, idxB = 0; idx < newArr.length; idx++, idxB++) {
+            newArr[idx] = b[idxB];
+        }
+
+
+        System.out.println(Arrays.toString(newArr));
+        System.out.printf("Length: a-%d; b-%d;%n", a.length, b.length);
+    }
+
+    private static <T> void mergeGenericArray(T[] a, T[] b) {
+        int[] newArr = new int[a.length + b.length];
+        int sizeA = a.length;
+
+        for (int indexA = 0, indexB = (-a.length), j = sizeA; indexA < newArr.length; indexA++, indexB++) {
+            if (indexA < a.length) {
+                T tmp = null;
+                newArr[indexA] = a[indexA];
+            } else {
+                newArr[sizeA++] = b[indexB];
+            }
+        }
+
+        System.out.println(Arrays.toString(newArr));
+        System.out.printf("Length: a-%d; b-%d;%n", a.length, b.length);
+    }
+}
