dd0026141d6714fa763a0d8c10030d360ba6a966
12z3 <77529497+12z3@users.noreply.github.com>
2022-11-07 07:43:48 +0200

    Add files via upload

--- Sorting.java
+++ Sorting.java
@@ -1,4 +1,4 @@
-package hackerRank;
+package task;
 
 import java.util.Arrays;
 
@@ -10,13 +10,49 @@ public class Sorting {
 
     public static void main(String[] args) {
 
+        /**
+         * ... ??? -> j < a.length - i - 1 -> Търси най-големият елемент в масива и го поставя в края му.
+         *         Изключва крайният елемент (j < a.length - i - 1) в следващото търсене (итерация на i).
+         </p>
+         *              [1, 15, 12, 3, 5, 7, 4, 9]
+         </p>
+         i = 0, j = 1:  [1, 12, 15, 3, 5, 7, 4, 9]  i = 0 -> j = [0.. 6]
+         i = 0, j = 2:  [1, 12, 3, 15, 5, 7, 4, 9]
+         i = 0, j = 3:  [1, 12, 3, 5, 15, 7, 4, 9]
+         i = 0, j = 4:  [1, 12, 3, 5, 7, 15, 4, 9]
+         i = 0, j = 5:  [1, 12, 3, 5, 7, 4, 15, 9]  ... на края на 5-та итерация и началото на 6-та.
+         i = 0, j = 6:  [1, 12, 3, 5, 7, 4, 9, 15]  ... на края на 6-та итерация и 7-ма няма.
+         i = 1, j = 1:  [1, 3, 12, 5, 7, 4, 9, 15]  i = 1 -> j = [0.. 5]
+         i = 1, j = 2:  [1, 3, 5, 12, 7, 4, 9, 15]
+         i = 1, j = 3:  [1, 3, 5, 7, 12, 4, 9, 15]
+         i = 1, j = 4:  [1, 3, 5, 7, 4, 12, 9, 15]
+         i = 1, j = 5:  [1, 3, 5, 7, 4, 9, 12, 15]
+         i = 2, j = 3:  [1, 3, 5, 4, 7, 9, 12, 15]  i = 2 -> j = [0.. 4]
+         i = 3, j = 2:  [1, 3, 4, 5, 7, 9, 12, 15]  i = 3 -> j = [0.. 3]
+         */
+
+
+        int[] a = {1, 15, 12, 3, 5, 7, 4, 9};
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - i - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int tmp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = tmp;
+                    System.out.println("i = " + i +
+                            ", j = " + j + ":  " + Arrays.toString(a)); 
+                }
+            }
+        }
+
         int[] arr = {7, 3, 8, 4, 5, 2};
         System.out.println(Arrays.toString(directSelectionSorting(arr)));
         System.out.println(Arrays.toString(directSelectionSorting1(arr)));
         System.out.println(Arrays.toString(bubbleSort(arr)));
     }
 
-    private static int[] directSelectionSorting(int[] arr) {
+    public static int[] directSelectionSorting(int[] arr) {
         int currentIndex = 0;
         while (currentIndex < arr.length) {
             int minIndex = findIndex(arr, currentIndex);
