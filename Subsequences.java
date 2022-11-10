6c07af35fde2d6ba8bd9faae97175eadb23b1ff7
12z3 <77529497+12z3@users.noreply.github.com>
2022-11-10 07:29:15 +0200

    Add files via upload

--- Subsequences.java
+++ Subsequences.java
@@ -18,15 +18,15 @@ public class Subsequences {
 
         int[] a2 = {1, 2, 3, 6, 7, 8};
         int[] a1 = {3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7, 8, 3, 4, 5, 6};
-        int[] a = {3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7, 8, 3, 4, 5, 6};
-        int[] a3 = {1, 2, 1, 2, 1, 2};
+        int[] a3 = {3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7, 8, 3, 4, 5, 6};
+        int[] a = {1, 2, 1, 2, 1, 2};
 
         consecutiveSubsequences(a);
     }
 
-    private  static void consecutiveSubsequences(int[] a){
+    private static void consecutiveSubsequences(int[] a) {
         List<Integer> ls;
-        int  count = 0;
+        int count = 0;
 
         for (int i = 0; i < a.length - 1; i++) {
             boolean PreviouslyAdded = false;
@@ -34,16 +34,12 @@ public class Subsequences {
             ls = new ArrayList<>();
             int el1 = a[i], el2 = a[i + 1],
                     index = i + 1, endEl = a.length - 1;
+            count++;
 
             while (compare(el1, el2) && index < endEl) {
-                if (PreviouslyAdded) {
-                    ls.add(el2);
-                    count++;
-                } else {
-                    ls.add(el1);
-                    ls.add(el2);
-                    count++;
-                }
+                if (!PreviouslyAdded) ls.add(el1);
+                ls.add(el2);
+                // count++;
 
                 PreviouslyAdded = true;
                 el1 = a[index];
@@ -54,13 +50,13 @@ public class Subsequences {
             if ((index == endEl) && (a[endEl] == a[endEl - 1] + 1)) {
                 if (!PreviouslyAdded) ls.add(a[endEl - 1]);
                 ls.add(a[endEl]);
-                count++;
             }
             System.out.println(ls);
         }
 
         System.out.println(count);
     }
+
     private static boolean compare(int el1, int el2) {
         return el1 + 1 == el2;
     }
