9826c415bb8941249de41015a2205e119e284b0f
12z3 <77529497+12z3@users.noreply.github.com>
2022-11-10 07:16:26 +0200

    Add files via upload

--- Subsequences.java
+++ Subsequences.java
@@ -28,7 +28,8 @@ public class Subsequences {
             boolean add = false;
 
             ls = new ArrayList<>();
-            int el1 = a[i], el2 = a[i + 1], index = i + 1, endEl = a.length - 1;
+            int el1 = a[i], el2 = a[i + 1],
+                    index = i + 1, endEl = a.length - 1;
 
             while (compare(el1, el2) && index < endEl) {
                 if (add) {
@@ -55,7 +56,6 @@ public class Subsequences {
         }
 
         System.out.println(count);
-//        System.out.println(ls);                          // [1, 2, 2, 3, 3, 4, 6, 7, 2, 3, 3, 4, 6, 7, 6, 7]
     }
 
     private static boolean compare(int el1, int el2) {
