1a9e655a7e74cb1f6542204963082701cd54ec66
12z3 <77529497+12z3@users.noreply.github.com>
2022-11-12 09:53:53 +0200

    Add files via upload

--- SearchDuplicates.java
+++ SearchDuplicates.java
@@ -3,19 +3,21 @@ package task;
 public class SearchDuplicates {
 
     /**
+     * <p>
      * Виж: GitHiB -> inTime -> RemoveDuplicateElements
      * {1, 1, 3, 3, 4, 5, 2, 1, 3, 4, 1};
      * {4, 0, 3, 0, 2, 1, 1, 0, 0, 0, 0}; - > counter of matches
-     * <p></p>
+     * </p>
      * Взима първият "1" и брои колко пъти се среща в следващите.
      * Взима втория. Проверява дали е проверяван. Ако Е прескача на следващия.
      * В противен случай извършва проверката и за него.
-     * <p></p>
+     * <p>
      * Element 1 = finds 4 times
      * Element 3 = finds 3 times
      * Element 4 = finds 2 times
      * Element 5 = finds 1 times
      * Element 2 = finds 1 times
+     * </p>
      */
 
     public static void main(String[] args) {
@@ -45,15 +47,15 @@ public class SearchDuplicates {
     }
 
     private static int findDuplicated(int[] arr, int el1, int index) {
-        int elCount = 1;
+        int matches = 1;
         while (index < arr.length) {
             int el2 = arr[index];
             if (el1 == el2) {
-                elCount++;
+                matches++;
             }
             index++;
         }
-        return elCount;
+        return matches;
     }
 
     private static boolean isChecked(int[] arr, int el, int index) {
