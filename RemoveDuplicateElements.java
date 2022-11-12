8a776e8e87ca95afe5f4970d7d8e3cd11efe02e3
12z3 <77529497+12z3@users.noreply.github.com>
2022-11-12 08:51:56 +0200

    Add files via upload

--- RemoveDuplicateElements.java
+++ RemoveDuplicateElements.java
@@ -1,13 +1,13 @@
 package hackerRank;
 
-import org.jetbrains.annotations.NotNull;
+
 import training.Methods;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class RemoveDuplicateElements extends Methods {
-    private static int @NotNull [] removeDuplicateElements(int[] arr) {
+    private static int [] removeDuplicateElements(int[] arr) {
         int countDuplicate = 0, index = 0;
         boolean isChecked = false;
         int[] checkedElements = new int[arr.length];
@@ -17,7 +17,7 @@ public class RemoveDuplicateElements extends Methods {
 
             if (i != 0) {
                 for (int j = 0; j < checkedElements.length; j++) {
-                    if (arr[i] == checkedElements[j]) {                            // Търси съвпадения
+                    if (arr[i] == checkedElements[j]) {                   // Търси съвпадения на текущия с предишните
                         isChecked = true;
                         break;
                     }
@@ -26,14 +26,14 @@ public class RemoveDuplicateElements extends Methods {
             if (isChecked) continue;                    // arr = {1, 2, 1, 1, 2, 2, 3, 4}
             // checkedElements[i] = arr[i];             // checkedElements = {1, 2, 0, 0, 0, 0, 3, 4} = non-duplicated
 
-            for (int j = i + 1; j < arr.length; j++) {             // Не стига до тук ако е проверяван елемента -
-                if (arr[i] == arr[j]) {                    // и има съвпадение -> if (arr[i] == checkedElements[j])
-                    countDuplicate++;
+            for (int j = i + 1; j < arr.length; j++) {     // Не стига до тук ако е проверяван елемента -
+                if (arr[i] == arr[j]) {                    // Първият елемент винаги минава през тази проверка.
+                    countDuplicate++;                      // Държи Общата бройка на повторенията
                 }
             }
             checkedElements[i] = arr[i];                    // checkedElements = {1, 2, 0, 0, 0, 0, 3, 4}
         }
-        int mach = 0;
+
         int[] tmp = new int[arr.length - countDuplicate];
 
         for (int i = 0; i < checkedElements.length; i++) {
@@ -41,16 +41,6 @@ public class RemoveDuplicateElements extends Methods {
                 tmp[index++] = checkedElements[i];
 
             }
-//        for (int i = 0; i < arr.length; i++) {
-//            mach = 1;
-//            for (int j = i + 1; j < arr.length; j++) {
-//                if (arr[i] == arr[j]) {
-//                    mach++;
-//                }
-//            }
-//            if (mach == 1) {
-//                tmp[index++] = arr[i];                 // Записва елементи с Индекси -> 1, 4, 5
-//            }                                          // Първо добавя елемента в tmp[l], след това увеличава л с 1.
         }
         arr = tmp;
         return arr;
