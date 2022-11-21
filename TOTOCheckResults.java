1d662be2761ad348f588d5335b86ec10cf1b44ba
12z3 <77529497+12z3@users.noreply.github.com>
2022-11-21 06:48:06 +0200

    Add files via upload

--- TOTOCheckResults.java
+++ TOTOCheckResults.java
@@ -10,7 +10,7 @@ public class TOTOCheckResults extends Toto {
         Scanner scanner = new Scanner(System.in);
         timeAndData();
 
-        //TODO: 92-и тираж:  9, 24, 26, 28, 37, 46
+        //TODO: 92-и тираж:  39, 24, 26, 28, 37, 46
 
         System.out.println("-----------------------------------------------");
         int[] a = officialResult("Резултат от тиража: ", scanner);
@@ -22,14 +22,14 @@ public class TOTOCheckResults extends Toto {
     }
 
     private static void question() {
-        List<Integer> last = new ArrayList<>(List.of(17, 21, 39, 41, 42, 44));
+        List<Integer> last = new ArrayList<>(List.of(12, 14, 17, 21, 39, 48));
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ще залагаме ли? (y / n) : ");
 
         String answer = scanner.nextLine();                         // while търси true
-        while (!answer.equalsIgnoreCase("y")            // true && true = true; true && false = false;
-                && !answer.equalsIgnoreCase("n")){
-            System.out.println(".... 'y' или 'n'?");
+        while (!answer.equalsIgnoreCase("y") &&         // true && true = true; true && false = false;
+                !answer.equalsIgnoreCase("n")) {
+            System.out.println("Айде сега.... 'y' или 'n'?");
             answer = scanner.nextLine();
         }
 
@@ -50,8 +50,8 @@ public class TOTOCheckResults extends Toto {
         String[] input = scanner.nextLine().
                 trim().
                 split(", ");
-        int[] result = new int[input.length];
 
+        int[] result = new int[input.length];
         for (int i = 0; i < result.length; i++) {
             result[i] = Integer.parseInt(input[i]);
         }
@@ -60,13 +60,13 @@ public class TOTOCheckResults extends Toto {
 
     private static void printResult(int[] a, int[] b) {
 
-        System.out.print("Имаш " + getCounts(a, b)+ " от " + a.length + " попадения: ");
+        System.out.print("Имаш " + getCounts(a, b) + " от " + a.length + " попадения: ");
 
         for (int i = 0; i < Objects.requireNonNull(compareResults(a, b)).length; i++) {
             if (Objects.requireNonNull(compareResults(a, b))[i] != 0 &&
                     i < Objects.requireNonNull(compareResults(a, b)).length - 1) {
-                System.out.print(a[i] + ", ");
-            } else System.out.print(a[i]);
+                System.out.print(Objects.requireNonNull(compareResults(a, b))[i] + ", ");
+            } else System.out.print(Objects.requireNonNull(compareResults(a, b))[i]);
         }
         System.out.println();
     }
@@ -81,14 +81,15 @@ public class TOTOCheckResults extends Toto {
 
     private static int[] getResult(int[] a, int[] b) {
         int[] tmp = new int[a.length];
+        boolean isMatch = false;
         for (int i = 0; i < a.length; i++) {
-            int count = 0;
+            isMatch = false;
             for (int j = 0; j < b.length; j++) {
                 if (a[i] == b[j]) {
-                    count++;
+                    isMatch = true;
                 }
             }
-            tmp[i] = count;
+            if (isMatch) tmp[i] = a[i];
         }
         return tmp;
     }
