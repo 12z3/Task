4790427af92d42a5191b2abcc09d93eec4c5480b
12z3 <77529497+12z3@users.noreply.github.com>
2022-11-22 22:39:21 +0200

    Add files via upload

--- TOTOCheckResultsNew.java
+++ TOTOCheckResultsNew.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class TOTOCheckResultsNew extends Toto {
 
-       // TODO: Пренапиши го грамотно използвайки ООП
+    // TODO: Пренапиши го грамотно използвайки ООП
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -15,21 +15,22 @@ public class TOTOCheckResultsNew extends Toto {
 
         // TODO: 92-и тираж:  12, 24, 26, 28, 37, 48  //  9, 24, 26, 28, 37, 46 - It's official;
 
-        letsGo(scanner);
+        letsGo();
     }
 
-    private static void letsGo(Scanner scanner) {
+    private static void letsGo() {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Проверяваме или играем? (c / p): ");
         String answerOne = scanner.nextLine().trim();
 
         if ("p".equalsIgnoreCase(checkAnswer(answerOne))) {
-            question();
-        } else if ("c".equalsIgnoreCase(checkAnswer(answerOne))) {
             play();
+        } else if ("c".equalsIgnoreCase(checkAnswer(answerOne))) {
+            checkResults();
         }
     }
 
-    private static void play() {
+    private static void checkResults() {
         System.out.println("-----------------------------------------------");
         int[] a = officialResult("Резултат от тиража: ");
         int[] b = yourSuppose("Твоят залог: ");
@@ -38,7 +39,7 @@ public class TOTOCheckResultsNew extends Toto {
         System.out.println("-----------------------------------------------");
     }
 
-    private static void question() {
+    private static void play() {
         List<Integer> last = new ArrayList<>(List.of(12, 14, 17, 21, 39, 48));
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ще залагаме ли? (y / n) : ");
@@ -57,6 +58,7 @@ public class TOTOCheckResultsNew extends Toto {
         }
         return answer;
     }
+
     private static String checkAnswerAgain() {
         Scanner scanner = new Scanner(System.in);
 
@@ -70,13 +72,11 @@ public class TOTOCheckResultsNew extends Toto {
     }
 
     private static int[] yourSuppose(String s) {
-        Scanner scanner = new Scanner(System.in);
         System.out.print(s);
         return result();
     }
 
     private static int[] officialResult(String s) {
-        Scanner scanner = new Scanner(System.in);
         System.out.print(s);
         return result();
     }
@@ -150,119 +150,119 @@ public class TOTOCheckResultsNew extends Toto {
 }
 
 /**
- public static void main(String[] args) {
- Scanner scanner = new Scanner(System.in);
- timeAndData();
-
- //TODO: 92-и тираж:  12, 24, 26, 28, 37, 48  // 9, 24, 26, 28, 37, 46
-
- System.out.print("Проверяваме или играем (c / p): ");
- String answerOne = scanner.nextLine().trim();
-
-
- if (answerOne.equalsIgnoreCase("p")){
- question();
- } else if (answerOne.equalsIgnoreCase("c")){
-
- System.out.println("-----------------------------------------------");
- int[] a = officialResult("Резултат от тиража: ", scanner);
- int[] b = yourSuppose("Твоят залог: ", scanner);
- System.out.println("-----------------------------------------------");
- printResult(a, b);
- System.out.println("-----------------------------------------------");
- }
- //question();
- }
-
- private static void question() {
- List<Integer> last = new ArrayList<>(List.of(12, 14, 17, 21, 39, 48));
- Scanner scanner = new Scanner(System.in);
- System.out.print("Ще залагаме ли? (y / n) : ");
-
- // while търси true;  // true && true = true; true && false = false;
- String answer = scanner.nextLine();
- while (!answer.equalsIgnoreCase("y") &&
- !answer.equalsIgnoreCase("n")) {
- System.out.println("Айде сега.... 'y' или 'n'?");
- answer = scanner.nextLine();
- }
-
- if (answer.equalsIgnoreCase("y")) generateTotoNum(last, 3, 5);
- }
-
- private static int[] yourSuppose(String s, Scanner scanner) {
- System.out.print(s);
- return result(scanner);
- }
-
- private static int[] officialResult(String s, Scanner scanner) {
- System.out.print(s);
- return result(scanner);
- }
-
- private static int[] result(Scanner scanner) {
- String[] input = scanner.nextLine().
- trim().
- split(", ");
-
- int[] result = new int[input.length];
- for (int i = 0; i < result.length; i++) {
- result[i] = Integer.parseInt(input[i]);
- }
- return result;
- }
-
- private static void printResult(int[] a, int[] b) {
-
- System.out.print("Имаш " + getCounts(a, b) + " от " + a.length + " попадения: ");
-
- int[] tmp = Objects.requireNonNull(compareResults(a, b));   // 12, 14, 17, 21, 39, 48
- int counter = getCounts(a,b);
- for (int i = 0; i < tmp.length; i++) {
- if (tmp[i] != 0 && counter > 1) {
- System.out.print(tmp[i] + ", ");
- counter--;
- } else if(tmp[i] != 0)  {
- System.out.print(tmp[i]);
- counter--;
- }
- if (counter == 0) break;
- }
- System.out.println();
- }
-
- private static int[] compareResults(int[] a, int[] b) {
- if (a.length != b.length) {
- System.out.println("ERROR");
- return null;
- }
- return getResult(a, b);
- }
-
- private static int[] getResult(int[] a, int[] b) {
- int[] tmp = new int[a.length];
- boolean isMatch = false;
- for (int i = 0; i < a.length; i++) {
- isMatch = false;
- for (int j = 0; j < b.length; j++) {
- if (a[i] == b[j]) {
- isMatch = true;
- }
- }
- if (isMatch) tmp[i] = a[i];
- }
- return tmp;
- }
-
- private static int getCounts(int[] a, int[] b) {
- int count = 0;
- for (int i = 0; i < a.length; i++) {
- for (int j = 0; j < b.length; j++) {
- if (a[i] == b[j]) {
- count++;
- }
- }
- }
- return count;
- }
+ * public static void main(String[] args) {
+ * Scanner scanner = new Scanner(System.in);
+ * timeAndData();
+ * <p>
+ * //TODO: 92-и тираж:  12, 24, 26, 28, 37, 48  // 9, 24, 26, 28, 37, 46
+ * <p>
+ * System.out.print("Проверяваме или играем (c / p): ");
+ * String answerOne = scanner.nextLine().trim();
+ * <p>
+ * <p>
+ * if (answerOne.equalsIgnoreCase("p")){
+ * question();
+ * } else if (answerOne.equalsIgnoreCase("c")){
+ * <p>
+ * System.out.println("-----------------------------------------------");
+ * int[] a = officialResult("Резултат от тиража: ", scanner);
+ * int[] b = yourSuppose("Твоят залог: ", scanner);
+ * System.out.println("-----------------------------------------------");
+ * printResult(a, b);
+ * System.out.println("-----------------------------------------------");
+ * }
+ * //question();
+ * }
+ * <p>
+ * private static void question() {
+ * List<Integer> last = new ArrayList<>(List.of(12, 14, 17, 21, 39, 48));
+ * Scanner scanner = new Scanner(System.in);
+ * System.out.print("Ще залагаме ли? (y / n) : ");
+ * <p>
+ * // while търси true;  // true && true = true; true && false = false;
+ * String answer = scanner.nextLine();
+ * while (!answer.equalsIgnoreCase("y") &&
+ * !answer.equalsIgnoreCase("n")) {
+ * System.out.println("Айде сега.... 'y' или 'n'?");
+ * answer = scanner.nextLine();
+ * }
+ * <p>
+ * if (answer.equalsIgnoreCase("y")) generateTotoNum(last, 3, 5);
+ * }
+ * <p>
+ * private static int[] yourSuppose(String s, Scanner scanner) {
+ * System.out.print(s);
+ * return result(scanner);
+ * }
+ * <p>
+ * private static int[] officialResult(String s, Scanner scanner) {
+ * System.out.print(s);
+ * return result(scanner);
+ * }
+ * <p>
+ * private static int[] result(Scanner scanner) {
+ * String[] input = scanner.nextLine().
+ * trim().
+ * split(", ");
+ * <p>
+ * int[] result = new int[input.length];
+ * for (int i = 0; i < result.length; i++) {
+ * result[i] = Integer.parseInt(input[i]);
+ * }
+ * return result;
+ * }
+ * <p>
+ * private static void printResult(int[] a, int[] b) {
+ * <p>
+ * System.out.print("Имаш " + getCounts(a, b) + " от " + a.length + " попадения: ");
+ * <p>
+ * int[] tmp = Objects.requireNonNull(compareResults(a, b));   // 12, 14, 17, 21, 39, 48
+ * int counter = getCounts(a,b);
+ * for (int i = 0; i < tmp.length; i++) {
+ * if (tmp[i] != 0 && counter > 1) {
+ * System.out.print(tmp[i] + ", ");
+ * counter--;
+ * } else if(tmp[i] != 0)  {
+ * System.out.print(tmp[i]);
+ * counter--;
+ * }
+ * if (counter == 0) break;
+ * }
+ * System.out.println();
+ * }
+ * <p>
+ * private static int[] compareResults(int[] a, int[] b) {
+ * if (a.length != b.length) {
+ * System.out.println("ERROR");
+ * return null;
+ * }
+ * return getResult(a, b);
+ * }
+ * <p>
+ * private static int[] getResult(int[] a, int[] b) {
+ * int[] tmp = new int[a.length];
+ * boolean isMatch = false;
+ * for (int i = 0; i < a.length; i++) {
+ * isMatch = false;
+ * for (int j = 0; j < b.length; j++) {
+ * if (a[i] == b[j]) {
+ * isMatch = true;
+ * }
+ * }
+ * if (isMatch) tmp[i] = a[i];
+ * }
+ * return tmp;
+ * }
+ * <p>
+ * private static int getCounts(int[] a, int[] b) {
+ * int count = 0;
+ * for (int i = 0; i < a.length; i++) {
+ * for (int j = 0; j < b.length; j++) {
+ * if (a[i] == b[j]) {
+ * count++;
+ * }
+ * }
+ * }
+ * return count;
+ * }
  */
