3b8a89da4d497286fc8a57ebee999d0ad3174554
12z3 <77529497+12z3@users.noreply.github.com>
2022-12-04 12:31:08 +0200

    Add files via upload

---
+++ CountWord.java
@@ -0,0 +1,42 @@
+package task;
+
+import java.util.Objects;
+
+public class CountWord {
+    public static void main(String[] args) {
+
+        /*
+        Колко пъти думата "word" се среща в текста "text"?
+        */
+
+        String text = "the aldks the dkjtlkee the";
+        String word = "the";
+
+        System.out.println(searchThisWord(text, word));
+        System.out.println(searchThisWord1(text, word));
+
+    }
+
+    private static int searchThisWord(String text, String word) {
+        int count = 0, index = 0;
+        for (int i = 0; i < text.length(); i++) {
+            int currentIndex = text.indexOf(word, index);
+            if (currentIndex != -1) {
+                count++;
+                index = currentIndex + word.length();
+            }
+        }
+        return count;
+    }
+
+    private static int searchThisWord1(String text, String word) {
+        int count = 0;
+        String[] tmp = text.split(" ");
+        for (int i = 0; i < tmp.length; i++) {
+            if (Objects.equals(tmp[i], word)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
