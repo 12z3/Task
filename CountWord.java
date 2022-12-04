e2135d22fdde5ed74ef2af5b9d4e5c1a005bd8d0
12z3 <77529497+12z3@users.noreply.github.com>
2022-12-04 12:50:08 +0200

    Add files via upload

--- CountWord.java
+++ CountWord.java
@@ -9,12 +9,34 @@ public class CountWord {
         Колко пъти думата "word" се среща в текста "text"?
         */
 
-        String text = "the aldks the dkjtlkee the";
-        String word = "the";
+        String text = "A regular expression (shortened as regex or regexp;[1] " +
+                "sometimes referred to as rational expression[2][3]) is a sequence " +
+                "of characters that specifies a search pattern in text. " +
+                "Usually such patterns are used by string-searching algorithms " +
+                "for \"find\" or \"find and replace\" operations on strings, " +
+                "or for input validation. Regular expression techniques are developed " +
+                "in theoretical computer science and formal language theory.\n" +
+                "\n" +
+                "The concept of regular expressions began in the 1950s, " +
+                "when the American mathematician " +
+                "Stephen Cole Kleene formalized the concept of a regular language. " +
+                "They came into common use with Unix text-processing utilities. " +
+                "Different syntaxes for writing regular expressions have existed since the 1980s, " +
+                "one being the POSIX standard and another, widely used, being the Perl syntax.";
 
+        String word = "the";
         System.out.println(searchThisWord(text, word));
         System.out.println(searchThisWord1(text, word));
+        System.out.println(searchThisWord2(text, word));
 
+    }
+
+    private static int searchThisWord2(String text, String word) {
+        int count = 0, nextIndex = -1;
+        while ((nextIndex = text.indexOf(word, nextIndex + 1)) != -1) {
+            count++;
+        }
+        return count;
     }
 
     private static int searchThisWord(String text, String word) {
@@ -33,9 +55,7 @@ public class CountWord {
         int count = 0;
         String[] tmp = text.split(" ");
         for (int i = 0; i < tmp.length; i++) {
-            if (Objects.equals(tmp[i], word)) {
-                count++;
-            }
+            if (tmp[i].equalsIgnoreCase(word)) count++;
         }
         return count;
     }
