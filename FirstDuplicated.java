a1de086d47e7b2ebaf959c48d6de18c3ca0ce3c9
12z3 <77529497+12z3@users.noreply.github.com>
2022-12-06 20:17:07 +0200

    Add files via upload

--- FirstDuplicated.java
+++ FirstDuplicated.java
@@ -10,21 +10,29 @@ public class FirstDuplicated {
         int[] a = {2, 1, 3, 1, 3, 2};    // 2 ?= 1 No -> 21 - 1 ?= 3 No -> 213
         int result = a[0], count = 0;
 
-        duplicate1(a, count);
+        duplicate1(a);
+        duplicate2(a);
     }
-    private static void duplicate2(int[] a, int count) {
-        List<Integer> list = new ArrayList<>();
+    private static void duplicate2(int[] a) {
+        int result = a[0], count = 0;
         for (int i = 0; i < a.length - 1; i++) {
-
-
+            if (result % 10 == a[i]) {
+                count++;
+            } else {
+                result = result / 10;
+            }
+            result *= 10;
+            result += a[i];
         }
         System.out.println(count);
     }
-    private static void duplicate1(int[] a, int count) {
+
+    private static void duplicate1(int[] a) {
+        int count = 0;
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < a.length - 1; i++) {
             list.add(a[i]);
-            if (list.contains(a[i+1])) count++;
+            if (list.contains(a[i + 1])) count++;
         }
         System.out.println(count);
     }
