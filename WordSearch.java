c3a97a884a0e51775396bc6d251d78c104121202
12z3 <77529497+12z3@users.noreply.github.com>
2022-12-20 14:49:51 +0200

    Add files via upload

--- WordSearch.java
+++ WordSearch.java
@@ -1,21 +1,32 @@
 package task;
 
-public class WordSearch {
+import training.Methods;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class WordSearch extends Methods {
 
     public static void main(String[] args) {
 
         String line = "astasta la vista babyastasta";
         String word = "asta";
+
+        String wordA = "lijap";
+        String wordB = "picas";
         char a = 'a';
 
-        System.out.printf
-                ("Word '%s' is matched %d times in text: %s%n",
-                        word, wordSearchInText(line, word), line);
-        System.out.printf("Char '%c' is matched %d times in text: %s",
-                a, charSearchInText(line, a), line);
+        wordSearchInText(line, word);
+        charSearchInText(line, a);
+
+        System.out.printf("Are that words same?: %s", compareTwoWords(wordA, wordB));
+
+        //System.out.println(compareWords(wordA, wordB));
     }
 
-    public static int wordSearchInText(String text, String word) {
+    public static void wordSearchInText(String text, String word) {
         int count = 0, index = 0, fromIndex = 0, j = 0;
 
         while (j < text.length()) {
@@ -26,10 +37,12 @@ public class WordSearch {
             }
             j++;
         }
-        return count;
+        System.out.printf
+                ("Word '%s' is matched %d times in text: %s%n",
+                        word, count, text);
     }
 
-    public static int charSearchInText(String text, char x) {
+    public static void charSearchInText(String text, char x) {
         int count = 0, index = 0, fromIndex = 0, j = 0;
         while (j < text.length()) {
             index = text.indexOf(x, fromIndex);
@@ -39,6 +52,39 @@ public class WordSearch {
             }
             j++;
         }
-        return count;
+        System.out.printf("Char '%c' is matched %d times in text: %s%n",
+                x, count, text);
+    }
+
+    public static boolean compareTwoWords(String wordA, String wordB) {
+        boolean isNoMatch = true;
+
+        Map<Integer, Character> different = new LinkedHashMap<>();
+        Stack<Character> aWord = new Stack<>();
+        Stack<Character> bWord = new Stack<>();
+
+        if (wordA.length() != wordB.length()) return false;
+
+        for (int i = 0; i < wordA.length(); i++) {
+            aWord.add(wordA.charAt(i));
+            bWord.add(wordB.charAt(i));
+        }
+
+        for (int i = 0; i < wordA.length(); i++) {
+            isNoMatch = true;
+            if (aWord.get(i) != bWord.get(i)) {
+                isNoMatch = false;
+                different.put(i, aWord.get(i));
+            }
+        }
+
+        if (!isNoMatch) {
+            System.out.println("Different are: ");
+            for (Map.Entry<Integer, Character> el : different.entrySet()) {
+                System.out.printf("Index: %d, Char: %c%n", el.getKey(), el.getValue());
+            }
+        }
+
+        return isNoMatch;
     }
 }
