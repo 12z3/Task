13e1376a5e6958139c2f2056ccbf82f0b7ac715e
12z3 <77529497+12z3@users.noreply.github.com>
2022-12-22 11:59:12 +0200

    Add files via upload

--- TotoPoint.java
+++ TotoPoint.java
@@ -11,7 +11,7 @@ public class TotoPoint {
             memo.add(generateNumbers());
         }
         for (List<Integer> el : memo) System.out.print(el + " ");
-        matchCheckers(memo, 11);
+        matchCheckers(memo, 111);
     }
 
     // Генерира резултат 1 @ count
@@ -26,16 +26,22 @@ public class TotoPoint {
     }
 
     // Генерира масив от 6 числа от 1 - 49
-    private static List<Integer> generateDigits() {
+    private static List<Integer> generateDigits() {                     // Тези трябва да са уникални едни спрямо други.
         Random rnd = new Random();
         List<Integer> a = new ArrayList<>();
-        int digit = 0;
 
         for (int i = 0; i < 6; i++) {
-            digit = rnd.nextInt(50);
-            if (digit != 0) {
-                a.add(digit);
-            } else i--;
+            int digit = rnd.nextInt(1, 50);
+            a.add(digit);
+            if (i > 0) {
+                for (int j = 0; j < a.size(); j++) {
+                    if (digit == a.get(j)) {
+                        digit = rnd.nextInt(1, 50);
+                        a.add(digit);                                    // Добави на поз. j и премахни от поз. j + 1
+                        a.remove(j + 1);
+                    }
+                }
+            }
         }
         //for (int el: a) System.out.print(el + " ");
         return a;
@@ -47,16 +53,24 @@ public class TotoPoint {
     private static List<List<Integer>> matchCheckers(List<List<Integer>> list, int el) {
         List<List<Integer>> matchers = new ArrayList<>();
         Random rnd = new Random();
+
         for (int i = 0; i < list.size(); i++) {
             List<Integer> matcher = new ArrayList<>();
             for (int j = 0; j < list.get(i).size(); j++) {
-                int digit = list.get(i).get(j);
-                if (digit != el) {
+                int digitA = list.get(i).get(j);
+                if (digitA != el) {
                     matcher.add(j, 0);
                 } else {
-                    digit = rnd.nextInt(1, 49);
-                    list.get(i).add(j, digit);                                    // Добавя digit на позиция j
-                    list.get(i).remove(j + 1);                              // Премахва 11 от позиция j + 1
+                    digitA = rnd.nextInt(1, 50);            // Добави на поз. j и премахни от поз. j + 1
+                    list.get(i).add(j, digitA);                                    // Добавя digit на позиция j
+                    list.get(i).remove(j + 1);                               // Премахва 11 от позиция j + 1
+                    for (int k = 0; k < list.get(i).size(); k++) {
+                        if (list.get(i).get(k) == digitA){
+                            digitA = rnd.nextInt(1, 50);
+                            list.get(i).add(j, digitA);                            // Добавя digit на позиция j
+                            list.get(i).remove(j + 1);                       // Премахва 11 от позиция j + 1
+                        }
+                    }
                     matcher.add(j, 1);
                 }
             }
