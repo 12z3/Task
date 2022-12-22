3db0bd2ebe3f3c8f8fbbdeb9facab1adcd315615
12z3 <77529497+12z3@users.noreply.github.com>
2022-12-22 16:54:47 +0200

    Add files via upload

--- TotoPoint.java
+++ TotoPoint.java
@@ -1,23 +1,34 @@
-package task;
+package task.TOTO;
 
+import hackerRank.Buffer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class TotoPoint {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        matchCheckers(getListsOfDigits(), 11);
+        //writeToFile(matchCheckers(memo,11));
+    }
+
+    private static List<List<Integer>> getListsOfDigits() {
         List<List<Integer>> memo = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             memo.add(generateNumbers());
         }
         for (List<Integer> el : memo) System.out.print(el + " ");
-        matchCheckers(memo, 111);
+        return memo;
     }
 
     // Генерира резултат 1 @ count
     private static List<Integer> generateNumbers() {
         List<Integer> numbers = new ArrayList<>();
-        int count = 5, step = 0;
+        int count = 10, step = 0;
         while (count >= 0) {
             numbers = generateDigits();
             count--;
@@ -25,33 +36,35 @@ public class TotoPoint {
         return numbers;
     }
 
-    // Генерира масив от 6 числа от 1 - 49
+    // Генерира масив от 6 числа по логика 1 от 49, след което срвнява генерираното с предишните числа.
+    // Бягаме от съвпадения.
     private static List<Integer> generateDigits() {                     // Тези трябва да са уникални едни спрямо други.
         Random rnd = new Random();
-        List<Integer> a = new ArrayList<>();
+        List<Integer> listOfDigits = new ArrayList<>();
 
         for (int i = 0; i < 6; i++) {
             int digit = rnd.nextInt(1, 50);
-            a.add(digit);
+            listOfDigits.add(digit);
             if (i > 0) {
-                for (int j = 0; j < a.size(); j++) {
-                    if (digit == a.get(j)) {
+                for (int j = 0; j < listOfDigits.size() - 1; j++) {
+                    if (digit == listOfDigits.get(j)) {
                         digit = rnd.nextInt(1, 50);
-                        a.add(digit);                                    // Добави на поз. j и премахни от поз. j + 1
-                        a.remove(j + 1);
+                        listOfDigits.add(digit);                                    // Добави на поз. j и премахни от поз. j + 1
+                        listOfDigits.remove(j + 1);
                     }
                 }
             }
         }
         //for (int el: a) System.out.print(el + " ");
-        return a;
+        return listOfDigits;
     }
 
     // [32, 17, 32, 27, 22, 9] [34, 47, 37, 11, 12, 36] [32, 36, 5, 34, 14, 46] -> 11
     // [0, 0, 0, 0, 0, 0]      [0, 0, 0, 1, 0, 0]       [0, 0, 0, 0, 0, 0]
     // [32, 17, 32, 27, 22, 9] [34, 47, 37, 22, 12, 36] [32, 36, 5, 34, 14, 46]
-    private static List<List<Integer>> matchCheckers(List<List<Integer>> list, int el) {
+    private static List<List<Integer>> matchCheckers(List<List<Integer>> list, int el) throws IOException {
         List<List<Integer>> matchers = new ArrayList<>();
+        List<List<Integer>> tmp = new ArrayList<>(list);
         Random rnd = new Random();
 
         for (int i = 0; i < list.size(); i++) {
@@ -65,7 +78,7 @@ public class TotoPoint {
                     list.get(i).add(j, digitA);                                    // Добавя digit на позиция j
                     list.get(i).remove(j + 1);                               // Премахва 11 от позиция j + 1
                     for (int k = 0; k < list.get(i).size(); k++) {
-                        if (list.get(i).get(k) == digitA){
+                        if (list.get(i).get(k) == digitA) {
                             digitA = rnd.nextInt(1, 50);
                             list.get(i).add(j, digitA);                            // Добавя digit на позиция j
                             list.get(i).remove(j + 1);                       // Премахва 11 от позиция j + 1
@@ -77,10 +90,33 @@ public class TotoPoint {
 
             matchers.add(i, matcher);
         }
+
         System.out.println();
         for (List<Integer> e : matchers) System.out.print(e + " ");
         System.out.println();
         for (List<Integer> e : list) System.out.print(e + " ");
+        writeToFile(list, matchers, tmp);
         return matchers;
+    }
+
+    private static void writeToFile(List<List<Integer>> list, List<List<Integer>> matchers, List<List<Integer>> tmp)
+            throws IOException {
+        BufferedWriter writer =
+                new BufferedWriter(new FileWriter("totoPoint.txt", true));
+
+        writer.newLine();
+        for (List<Integer> el : tmp) {
+            writer.write(el.toString());
+        }
+        writer.newLine();
+        for (List<Integer> el : matchers) {
+            writer.write(el.toString());
+        }
+        writer.newLine();
+        for (List<Integer> el : list) {
+            writer.write(el.toString());
+        }
+        writer.newLine();
+        writer.close();
     }
 }
