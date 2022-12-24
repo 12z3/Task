c20cca56fd3ada829d7da2c405a0bb6e8bc8c1e4
12z3 <77529497+12z3@users.noreply.github.com>
2022-12-24 13:16:29 +0200

    Add files via upload

---
+++ MainTOTO.java
@@ -0,0 +1,12 @@
+package task.TOTO.Projects;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MainTOTO {
+    public static void main(String[] args) throws IOException {
+
+        TOTO toto = new TOTO();
+        toto.totoPlay();
+    }
+}
