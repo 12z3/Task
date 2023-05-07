5f744179003027a3f748aebda56bb7368efaf70a
12z3 <77529497+12z3@users.noreply.github.com>
2023-05-07 16:16:32 +0300

    Add files via upload

--- StringTask.java
+++ StringTask.java
@@ -1,15 +1,55 @@
 package task;
 
+import hackerRank.Collections;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 public class StringTask {
     public static void main(String[] args) {
         String word = "ice", word1 = "key";
         String text = "icekicealkiicecelice", text1 = "kkeyeytextkey";
+        List<String> list = new ArrayList<>(List.of("Windows", "Linux"));
+        String text2 = "Linux offers great speed and security, " +
+                "on the other hand, Windows offers great ease of use, " +
+                "so that even non-tech-savvy people can work easily on personal computers. " +
+                "Linux is employed by many corporate organizations as servers and " +
+                "OS for security purpose while Windows is mostly employed by business users and gamers.";
+        String wordA = "Linux", wordB = "Windows";
 
         //textA(word, text);
         System.out.println();
         textB(word1, text1);
         System.out.println();
-        textC(word,text);
+        textC(word, text);
+        System.out.println(wordReplacement(wordReplacement(text2, wordA), wordB));
+        System.out.println(wordReplacementA(text2, list));
+    }
+
+    private static void printStreamReplacementWord(String text) {
+        String[] input = text.split(" ");
+
+
+    }
+
+    private static String wordReplacement(String text, String word) {
+        StringBuilder replacementWord = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) replacementWord.append("*");
+        while (text.contains(word)) text = text.replace(word, replacementWord);
+        return text;
+    }
+
+    private static String wordReplacementA(String text, List<String> bannedWords) {
+        StringBuilder replacementWord = new StringBuilder();
+        for (String banned : bannedWords) {
+            replacementWord.append("*".repeat(banned.length()));
+            text = text.replace(banned, replacementWord);
+            replacementWord.delete(0, replacementWord.length());
+        }
+        return text;
     }
 
     private static void textC(String word, String text) {
