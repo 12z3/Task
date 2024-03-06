4e699f8045eda219a0e8631b23b1ee8dc8307862
12z3 <77529497+12z3@users.noreply.github.com>
2024-03-06 13:08:21 +0200

    Add files via upload

--- Finance.java
+++ Finance.java
@@ -8,62 +8,54 @@ import java.util.regex.Pattern;
 
 public class Finance {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("");
+        File file = new File("/Users/blagojnikolov/Documents/VSCode/reportDSK3.csv");
         List<List<String>> data = readData(file);
 
         // printAllInfo(data);
-        // findByType(data, "02.01.2024");
-        findByType(data, "ЗАПЛАТА");
-        findByType(data, "АВАНС");
-        findByType(data, "ЕВН");
-        findByType(data, "ЕЛЕКТРОХОЛД");
-        findByType(data, "ЕЛ ЕНЕРГИЯ");
-        findByType(data, "VIVACOM");
-        findByType(data, "ПЛАЩАНЕ ПО ЗАЕМ");
-        findByType(data, "ЛИХВА ОВЪРДРАФТ");
-        findByType(data, "ИНТЕРНЕТ");
-        findByType(data, "BREZOVO");
-        findByType(data, "SMS/VIBER");
-        findByType(data, "SMS/VIBER");
-
+        printTransactions(data);
+        printAllDebit(data);
+        printAllWithdrawal(data);
     }
 
     /*
-     * matcher.find() се използва за намиране на всички съвпадения, и за
-     * всяко съвпадение,
-     * извлеченият стринг (без кавичките) се добавя в масива с
-     * words.add(matcher.group(1)).
-     *
-     * List<String>,съдържа всички думи от входния стринг, които бяха в кавички,
-     * и можете да се отпечата или да обработи този масив по желание.
-     *
+     * Израза \"([^\"]*)\"
+     * търси за сегменти от текст, които започват и завършват с двойна кавичка,
+     * като изключва самите двойни кавички от съвпадението и улавя всичко между тях
+     * като група
      * Debit = [9] = "121,21"; Withdrawal = [10] = "23,00"; Date = [11]; Time =
      * [12];
      */
 
-    protected static List<List<String>> readData(File file) throws FileNotFoundException {
+    protected static List<List<String>> readData(File file) {
         Pattern pattern = Pattern.compile("\"([^\"]*)\"");
-        Scanner scanner = new Scanner(file);
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("НЯМА ТАКЪВ ФАЙЛ... ?");
+        }
         List<List<String>> result = new ArrayList<>();
         List<String> string = null;
 
         int cnt = 0;
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            if (cnt >= 15) {
-                string = new ArrayList<>();
-                Matcher matcher = pattern.matcher(line);
-                while (matcher.find())
-                    string.add(matcher.group());
-                result.add(string);
+        if (scanner != null) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (cnt >= 15) {
+                    string = new ArrayList<>();
+                    Matcher matcher = pattern.matcher(line);
+                    while (matcher.find())
+                        string.add(matcher.group());
+                    result.add(string);
+                }
+                cnt++;
             }
-            cnt++;
         }
         return result;
     }
 
     protected static Double getDebit(List<List<String>> data, int numOfRow) {
-        String str = data.get(numOfRow).get(9).replaceAll("\"", "").trim();
+        String str = data.get(numOfRow).get(10).replaceAll("\"", "").trim();
         str = str.replace(",", ".");
         return (str.isEmpty()) ? Math.ceil(Double.parseDouble("0.00")) : Math.ceil(Double.parseDouble(str));
     }
@@ -86,7 +78,7 @@ public class Finance {
 
     protected static Double getWithdrawal(List<List<String>> data, int numOfRow) {
 
-        String str = data.get(numOfRow).get(10).replaceAll("\"", "").trim();
+        String str = data.get(numOfRow).get(9).replaceAll("\"", "").trim();
         str = str.replace(",", ".");
         return (str.isEmpty()) ? Math.ceil(Double.parseDouble("0.00")) : Math.ceil(Double.parseDouble(str));
     }
@@ -95,46 +87,61 @@ public class Finance {
         System.out.println();
         double withdrawal = 0.0, allWithdrawal = 0.0;
         String transaction = null, date = null, hour = null;
-        for (int i = 0, cnt = 0; i < data.size(); i++, cnt++) {
-            withdrawal = getWithdrawal(data, i);
+
+        System.out.println("Баланс: " + (allDebit(data) - allWithdrawal(data) + "BGN"));
+
+        for (int row = 0, cnt = 0; row < data.size(); row++, cnt++) {
+            withdrawal = getWithdrawal(data, row);
             allWithdrawal += withdrawal;
-            transaction = getTransaction(data, i);
-            date = getDate(data, i);
-            hour = getHour(data, i);
+            transaction = getTransaction(data, row);
+            date = getDate(data, row);
+            hour = getHour(data, row);
             if (cnt == 0) {
                 spaces(179, (date + hour + transaction));
                 System.out.println("Изтеглени:");
             }
-            System.out.printf("%s / %s: - %s", date, hour, transaction);
-            spaces(125, (date + hour + transaction));
-            System.out.printf("%.2f%n", withdrawal);
+            if (withdrawal != 0.0) {
+                System.out.printf("%s / %s: - %s", date, hour, transaction);
+                spaces(125, (date + hour + transaction));
+                System.out.printf("%.2f%n", withdrawal);
+            }
         }
-        System.out.println();
-        spaces(178, (date + hour + transaction));
-        System.out.printf("%.2f ", allWithdrawal);
+        if (allWithdrawal != 0.0) {
+            System.out.println();
+            spaces(179, (date + hour + transaction));
+            System.out.printf("%.2f ", allWithdrawal);
+        }
     }
 
     protected static void printAllDebit(List<List<String>> data) {
         System.out.println();
         double debit = 0.0, allDebit = 0.0;
         String transaction = null, date = null, hour = null;
-        for (int i = 0, cnt = 0; i < data.size(); i++, cnt++) {
-            debit = getDebit(data, i);
+
+        System.out.println("Баланс: " + (allDebit(data) - allWithdrawal(data) + "BGN"));
+
+        for (int row = 0, cnt = 0; row < data.size(); row++, cnt++) {
+            debit = getDebit(data, row);
             allDebit += debit;
-            transaction = getTransaction(data, i);
-            date = getDate(data, i);
-            hour = getHour(data, i);
+            transaction = getTransaction(data, row);
+            date = getDate(data, row);
+            hour = getHour(data, row);
             if (cnt == 0) {
                 spaces(179, (date + hour + transaction));
                 System.out.println("Внесени:");
             }
-            System.out.printf("%s / %s: - %s", date, hour, transaction);
-            spaces(125, (date + hour + transaction));
-            System.out.printf("%.2f%n", debit);
+            if (debit != 0.0) {
+                System.out.printf("%s / %s: - %s", date, hour, transaction);
+                spaces(125, (date + hour + transaction));
+                System.out.printf("%.2f%n", debit);
+            }
         }
-        System.out.println();
-        spaces(178, (date + hour + transaction));
-        System.out.printf("%.2f ", allDebit);
+        if (allDebit != 0.0) {
+            System.out.println();
+            spaces(179, (date + hour + transaction));
+            System.out.printf("%.2f ", allDebit);
+        }
+
     }
 
     protected static String getDate(List<List<String>> data, int numOfRow) {
@@ -149,46 +156,85 @@ public class Finance {
         return data.get(numOfRow).get(2);
     }
 
+    // protected static void findByType(List<List<String>> data, String type) {
+    // System.out.println();
+    // double debit = 0, withdrawal, allDebit = 0.0, allWithdrawal = 0.0;
+    // String transactions = null, hour = null,
+    // wordA = null, wordB = null, date = null;
+    // int spacesA = 0, spacesB = 0, constA = 0, constB = 0;
+    //
+    // Pattern datePattern = Pattern.compile(type);
+    //
+    // for (int i = 0; i < data.size(); i++) {
+    // Matcher dateMatcher = datePattern.matcher(String.valueOf(data.get(i)));
+    // if (dateMatcher.find()) {
+    // transactions = getTransaction(data, i);
+    // debit = getDebit(data, i);
+    // allDebit += Math.ceil(debit);
+    // withdrawal = getWithdrawal(data, i);
+    // allWithdrawal += Math.ceil(withdrawal);
+    // date = getDate(data, i);
+    // hour = getHour(data, i);
+    //
+    // wordA = (type + ": " + date + " " + transactions + " - " + hour);
+    // System.out.printf("%s: %s %s - %s", type, date, transactions, hour);
+    //
+    // constA = 129;
+    // spacesA = constA - wordA.length();
+    // spaces(constA, wordA);
+    // System.out.printf("Изтеглени: %.2f", Math.ceil(debit));
+    //
+    // spaces(18, "Изтеглени " + debit);
+    // System.out.printf("Постъпили: %.2f%n", Math.ceil(withdrawal));
+    // }
+    // }
+    // }
+
     protected static void findByType(List<List<String>> data, String type) {
         System.out.println();
         double debit = 0, withdrawal, allDebit = 0.0, allWithdrawal = 0.0;
-        String transactions = null, hour = null,
-                wordA = null, wordB = null, date = null;
-        int spacesA = 0, spacesB = 0, constA = 0, constB = 0;
+        String transactions = null, hour = null, date = null,
+                spacesWordA = null, spacesWordB = null, spacesWordC, spacesWordD;
+        int idxA = 0, idxB = 0;
 
-        Pattern datePattern = Pattern.compile(type);
+        Pattern typePattern = Pattern.compile(type);
 
-        for (int i = 0; i < data.size(); i++) {
-            Matcher dateMatcher = datePattern.matcher(String.valueOf(data.get(i)));
-            if (dateMatcher.find()) {
-                transactions = getTransaction(data, i);
-                debit = getDebit(data, i);
+        int maxLength = maxLength(data);
+        for (int row = 0; row < data.size(); row++) {
+            Matcher typeMatcher = typePattern.matcher(String.valueOf(data.get(row)));
+            if (typeMatcher.find()) {
+                transactions = getTransaction(data, row);
+                debit = getDebit(data, row);
                 allDebit += Math.ceil(debit);
-                withdrawal = getWithdrawal(data, i);
+                withdrawal = getWithdrawal(data, row);
                 allWithdrawal += Math.ceil(withdrawal);
-                date = getDate(data, i);
-                hour = getHour(data, i);
+                date = getDate(data, row);
+                hour = getHour(data, row);
 
-                wordA = (type + ": " + date + " " + transactions + " - " + hour);
-                System.out.printf("%s: %s %s - %s", type, date, transactions, hour);
+                System.out.printf("%s: %s %s - %s", type, date, hour, transactions);
+                spacesWordA = (type + date + hour + transactions);
+                idxA = spacesA(maxLength, spacesWordA, 25);
+                System.out.printf("Постъпили: %.2f", Math.ceil(debit));
 
-                constA = 129;
-                spacesA = constA - wordA.length();
-                spaces(constA, wordA);
-                System.out.printf("Изтеглени: %.2f", Math.ceil(debit));
-
-                spaces(18, "Изтеглени " + debit);
-                System.out.printf("Постъпили: %.2f%n", Math.ceil(withdrawal));
+                spacesWordB = ("Постъпили " + debit);
+                idxB = spacesA(-1, spacesWordB, 20);
+                System.out.printf("Изтеглени: %.2f%n", Math.ceil(withdrawal));
             }
         }
 
-        spaces(-spacesA - 17, transactions + " " + hour + type + "Изтеглени: ");
+        spacesWordC = ("Постъпили ");
+        spacesA(maxLength + 46, spacesWordC, 6);
         System.out.printf("%.2f", allDebit);
 
-        constB = constA - 99;
-        wordB = ("Изтеглени: " + allDebit);
-        spaces(constB, wordB);
+        spacesWordD = ("Изтеглени: " + allDebit);
+        spacesA(0, spacesWordD, 31);
         System.out.printf("%.2f%n", allWithdrawal);
+    }
+
+    protected static void generateWord(int maxLength) {
+        for (int i = 0, cnt = 0; i < maxLength; i++, cnt++) {
+
+        }
     }
 
     protected static void printAllInfo(List<List<String>> data) {
@@ -235,9 +281,45 @@ public class Finance {
         }
     }
 
+    protected static int spacesB(int num) {
+        int cnt;
+        for (cnt = 0; cnt < num; cnt++) {
+            System.out.print(" ");
+        }
+        return cnt;
+    }
+
+    static int spacesA(int maxLength, String currentLineLength, int spaces) {
+        int cnt = 0;
+        int numSpaces = (maxLength - currentLineLength.length()) + spaces;
+        for (cnt = 0; cnt < numSpaces; cnt++) {
+            System.out.print(" ");
+
+        }
+        return maxLength + currentLineLength.length() + spaces + cnt;
+    }
+
+    static int maxLength(List<List<String>> list) {
+        int maxLength = Integer.MIN_VALUE;
+        for (List<String> strings : list) {
+            if (strings.get(2).length() > maxLength)
+                maxLength = strings.get(2).length();
+        }
+        return maxLength;
+    }
+
     protected static <T> void printList(List<T> list) {
         for (T el : list) {
             System.out.println(el);
         }
+    }
+
+    protected static void printTransactions(List<List<String>> data) {
+        List<String> types = new ArrayList<>(List.of("ЗАПЛАТА", "АВАНС", "ЕВН", "ЕЛЕКТРОХОЛД", "ЕЛ ЕНЕРГИЯ",
+                "VIVACOM", "ПЛАЩАНЕ ПО ЗАЕМ", "ЛИХВА ОВЪРДРАФТ",
+                "ИНТЕРНЕТ", "BREZOVO", "VIBER"));
+
+        for (String type : types)
+            findByType(data, type);
     }
 }
