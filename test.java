d02b0961011bc0430d73885d3255a2ac8ea10d4a
12z3 <77529497+12z3@users.noreply.github.com>
2024-03-20 08:58:00 +0200

    Add files via upload

--- test.java
+++ test.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
@@ -12,37 +11,42 @@ public class test {
 //        }
 
         //String input = "1 1 1 2 2 3 3 3 4 4 5 6 7 8";
-        String input = "1 2 1 2 1 3 4 3 4 3 5 6 7 8";
+        String input = "1 3 1 2 1 3 4 3 4 3 5 6 2 8 3";
         frequentNumbers(input);
     }
 
 
-    protected static void frequentNumbers(String input){
+    protected static void frequentNumbers(String input) {
+        int cnt;
         String[] numbers = input.split(" ");
         int[] countMatches = new int[numbers.length];
-        String[] digits = new String[numbers.length];
-        int cnt = 0;
         System.out.println(Arrays.toString(numbers));
+
+        String[] digits = new String[numbers.length];
+        Arrays.fill(digits, "-1");
 
         for (int i = 0; i < numbers.length; i++) {
             cnt = 1;
-            if (digits[i] != null && checkDigit(digits, numbers[i])) continue;
+            if (isChecked(digits, numbers[i])) continue;
             for (int j = i + 1; j < numbers.length; j++) {
                 if (numbers[i].equalsIgnoreCase(numbers[j])) cnt++;
             }
-            countMatches[i] = cnt;
             digits[i] = numbers[i];
+            countMatches[i] = cnt;
+        }
+        for (int i = 0; i < countMatches.length; i++) {
+            if (countMatches[i] != 0) System.out.printf("%s: %s%n", numbers[i], countMatches[i]);
         }
 
-        System.out.println(Arrays.toString(countMatches));
         System.out.println(Arrays.toString(digits));
+        System.out.println(Arrays.toString(countMatches));
     }
 
-    protected static boolean checkDigit(String[] arr, String el){
+    protected static boolean isChecked(String[] arr, String el) {
         for (String string : arr) {
             if (string.equalsIgnoreCase(el)) return true;
         }
-        return  false;
+        return false;
     }
 
     public static void insertionSort(int[] arr) {
